8231f975df68aaf1f6e9728e42b9b3d5878921ee
FreddyShipman <jose.guzman252524@potros.itson.edu.mx>
2025-10-13 18:31:38 -0700

    Se agregaron las entidades del proyecto, no tienen codigo aun
    Entidades del MER propuesto, solo agregue las entidades al package y le deje a cada uno el id, esto para modificarlo despues uno a uno.

---
+++ src/main/java/Entidades/Interaccion.java
@@ -0,0 +1,20 @@
+package Entidades;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Fred
+ */
+@Entity
+public class Interaccion implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;   
+}
