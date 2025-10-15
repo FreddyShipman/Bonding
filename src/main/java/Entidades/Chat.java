02f57d46b1dca38c9362c6db5d1256ac677a8477
AlexNieblas10 <elalexnieblas@gmail.com>
2025-10-15 14:53:19 -0700

    feat: crear entidad chat

--- src/main/java/Entidades/Chat.java
+++ src/main/java/Entidades/Chat.java
@@ -1,13 +1,18 @@
 package Entidades;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
 /**
- *
  * @author Fred
  */
 @Entity
@@ -16,5 +21,54 @@ public class Chat implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long idChat;
+
+    private LocalDateTime fechaCreacion;
+
+    @OneToOne
+    @JoinColumn(name = "idMatch")
+    private Match match;
+
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL)
+    private List<Mensaje> mensajes;
+
+    public Chat() {
+    }
+
+    public Chat(Long idChat, LocalDateTime fechaCreacion) {
+        this.idChat = idChat;
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Long getIdChat() {
+        return idChat;
+    }
+
+    public void setIdChat(Long idChat) {
+        this.idChat = idChat;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Match getMatch() {
+        return match;
+    }
+
+    public void setMatch(Match match) {
+        this.match = match;
+    }
+
+    public List<Mensaje> getMensajes() {
+        return mensajes;
+    }
+
+    public void setMensajes(List<Mensaje> mensajes) {
+        this.mensajes = mensajes;
+    }
 }
