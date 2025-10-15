46d70f41bfc0354393347bb77de69b293930f5f9
AlexNieblas10 <elalexnieblas@gmail.com>
2025-10-15 15:09:35 -0700

    refactor: actualizar Bonding.java

--- src/main/java/Main/Bonding.java
+++ src/main/java/Main/Bonding.java
@@ -1,8 +1,8 @@
 package Main;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 /**
  *
@@ -14,8 +14,15 @@ public class Bonding {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("BondingPU");
-        EntityManager em = emf.createEntityManager();
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("BondingPU");
+             EntityManager em = emf.createEntityManager()) {
+
+            System.out.println("Conexión exitosa - Tablas creadas/actualizadas");
+
+        } catch (Exception e) {
+            System.err.println("Error al conectar con la base de datos: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
     
 }
